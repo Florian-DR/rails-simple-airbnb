582795116b8156f65f33c8c77fc72471cd8f5bbf
Florian de Radiguès <florian.radigues@gmail.com>
2023-02-22 18:04:43 +0100

    until new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Flat.destroy_all
+
+10.times do
+  flat = Flat.create!(
+    name: 'Light & Spacious Garden Flat London',
+    address: '10 Clifton Gardens London W9 1DT',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: rand(50..1000),
+    number_of_guests: rand(2..12)
+  )
+  puts "Creating flat number #{flat.id}.."
+end
